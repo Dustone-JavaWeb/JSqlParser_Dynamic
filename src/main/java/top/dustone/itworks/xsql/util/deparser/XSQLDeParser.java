d0cc3ebfb9c6ede0fdb2824ab79e185423d665f0
cheny426 <cheny426@vanke.com>
2022-04-22 12:44:03 +0800

    Select 定型

--- src/main/java/top/dustone/itworks/xsql/util/deparser/XSQLDeParser.java
+++ src/main/java/top/dustone/itworks/xsql/util/deparser/XSQLDeParser.java
@@ -1,3 +1,12 @@
+/*-
+ * #%L
+ * JSQLParser library
+ * %%
+ * Copyright (C) 2004 - 2022 JSQLParser
+ * %%
+ * Dual licensed under GNU LGPL 2.1 or Apache License 2.0
+ * #L%
+ */
 package top.dustone.itworks.xsql.util.deparser;
 
 import net.sf.jsqlparser.util.deparser.ExpressionDeParser;
